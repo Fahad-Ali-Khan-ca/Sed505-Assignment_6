b38dee18815bd9e8ccc312ebd1097ac873d1ebd4
Fahad Ali Khan <97141637+T-H-E-SHIROYASHA@users.noreply.github.com>
2023-12-08 21:25:01 -0500

    Update main.cpp

--- ComplexCode/main.cpp
+++ ComplexCode/main.cpp
@@ -2,6 +2,11 @@
 #include "InputData.h"
 #include "SentimentAnalysisModel.h"
 #include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+
 /*
 Complex Code Problem: Feature Hashing for Text Classification
 Problem Statement:
@@ -11,33 +16,38 @@ performance due to the curse of dimensionality. For instance, processing user re
 vocabulary with potentially millions of unique words.
 
 How the Hash Feature Pattern Solves the Problem:
-The Hash Feature pattern addresses this issue by transforming the high-dimensional 
+The Hash Feature pattern addresses this issue by transforming the high-dimensional
 word features into a lower-dimensional space using a hash function.
-This process, known as feature hashing or the hashing trick, 
+This process, known as feature hashing or the hashing trick,
 significantly reduces the feature space, which in turn decreases memory usage and speeds up the training process.
 
 */
+
 int main() {
     // Example input data with potential user reviews
     std::vector<std::string> reviews = {
         "I love this product", "Hated the experience", "Best purchase ever", "Not worth the price"
     };
 
-    // Initialize the input data handler
+    // Initialize the input data handler and process the reviews
     InputData inputData(reviews);
+    std::vector<std::vector<std::string>> processedData = inputData.getProcessedData();
 
-    // Processed data
-    std::vector<std::string> processedData = inputData.getProcessedData();
-
-    // Initialize the hash function with a bucket size
-    size_t bucketSize = 100;  // For demonstration
+    // Initialize the hash function with a chosen bucket size
+    size_t bucketSize = 1000;  // Larger bucket size for a more realistic scenario
     HashFunction hashFunc(bucketSize);
 
     // Hash the processed data
-    std::vector<size_t> hashedFeatures;
-    for (const auto& review : processedData) {
-        hashedFeatures.push_back(hashFunc(review));
+    std::unordered_set<size_t> uniqueHashes; // Use a set to avoid duplicate hash values
+    for (const auto& tokenVector : processedData) {
+        for (const auto& token : tokenVector) {
+            size_t hashValue = hashFunc(token);
+            uniqueHashes.insert(hashValue); // Insert the hash value into the set
+        }
     }
+
+    // Convert the set of unique hash values into a vector for the model
+    std::vector<size_t> hashedFeatures(uniqueHashes.begin(), uniqueHashes.end());
 
     // Initialize the sentiment analysis model and train it with hashed features
     SentimentAnalysisModel model;
